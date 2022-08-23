352ebce0adef5c8ff2318b755d88a156e4833110
yumataesu <yuma.taesu@gmail.com>
2022-08-23 12:20:02 +0900

    update : LSP kaymap

--- .config/nvim/lua/plugins.lua
+++ .config/nvim/lua/plugins.lua
@@ -27,34 +27,32 @@ packer.startup(function(use)
 end)
 
 -- 1. LSP Sever management
--- keyboard shortcut
--- vim.keymap.set('n', 'K',  '<cmd>lua vim.lsp.buf.hover()<CR>')
 vim.cmd([[autocmd BufWritePost init.lua source <afile> | PackerCompile]])
 
 local on_attach = function(client, bufnr)
 
   -- LSPサーバーのフォーマット機能を無効にするには下の行をコメントアウト
   -- 例えばtypescript-language-serverにはコードのフォーマット機能が付いているが代わりにprettierでフォーマットしたいときなどに使う
-  client.resolved_capabilities.document_formatting = false
+  -- client.resolved_capabilities.document_formatting = false
 
   local set = vim.keymap.set
   set("n", "gD", "<cmd>lua vim.lsp.buf.declaration()<CR>")
   set("n", "gd", "<cmd>lua vim.lsp.buf.definition()<CR>")
   set("n", "K", "<cmd>lua vim.lsp.buf.hover()<CR>")
-  set("n", "gi", "<cmd>lua vim.lsp.buf.implementation()<CR>")
-  set("n", "<C-k>", "<cmd>lua vim.lsp.buf.signature_help()<CR>")
-  set("n", "<space>wa", "<cmd>lua vim.lsp.buf.add_workspace_folder()<CR>")
-  set("n", "<space>wr", "<cmd>lua vim.lsp.buf.remove_workspace_folder()<CR>")
-  set("n", "<space>wl", "<cmd>lua print(vim.inspect(vim.lsp.buf.list_workspace_folders()))<CR>")
-  set("n", "<space>D", "<cmd>lua vim.lsp.buf.type_definition()<CR>")
-  set("n", "<space>rn", "<cmd>lua vim.lsp.buf.rename()<CR>")
-  set("n", "<space>ca", "<cmd>lua vim.lsp.buf.code_action()<CR>")
-  set("n", "gr", "<cmd>lua vim.lsp.buf.references()<CR>")
-  set("n", "<space>e", "<cmd>lua vim.lsp.diagnostic.show_line_diagnostics()<CR>")
-  set("n", "[d", "<cmd>lua vim.lsp.diagnostic.goto_prev()<CR>")
-  set("n", "]d", "<cmd>lua vim.lsp.diagnostic.goto_next()<CR>")
-  set("n", "<space>q", "<cmd>lua vim.lsp.diagnostic.set_loclist()<CR>")
-  set("n", "<space>f", "<cmd>lua vim.lsp.buf.formatting()<CR>")
+  set("n", "<C-k>j", "<cmd>lua vim.lsp.buf.implementation()<CR>")
+--  set("n", "<C-k>", "<cmd>lua vim.lsp.buf.signature_help()<CR>")
+--  set("n", "<C-w>a", "<cmd>lua vim.lsp.buf.add_workspace_folder()<CR>")
+--  set("n", "<space>wr", "<cmd>lua vim.lsp.buf.remove_workspace_folder()<CR>")
+--  set("n", "<space>wl", "<cmd>lua print(vim.inspect(vim.lsp.buf.list_workspace_folders()))<CR>")
+  set("n", "<C-k>D", "<cmd>lua vim.lsp.buf.type_definition()<CR>")
+  set("n", "<C-k>r", "<cmd>lua vim.lsp.buf.rename()<CR>")
+  set("n", "<C-k>a", "<cmd>lua vim.lsp.buf.code_action()<CR>")
+  set("n", "<C-k>s", "<cmd>lua vim.lsp.buf.references()<CR>")
+--  set("n", "<C-e>", "<cmd>lua vim.lsp.diagnostic.show_line_diagnostics()<CR>")
+--  set("n", "[d", "<cmd>lua vim.lsp.diagnostic.goto_prev()<CR>")
+--  set("n", "]d", "<cmd>lua vim.lsp.diagnostic.goto_next()<CR>")
+--  set("n", "<space>q", "<cmd>lua vim.lsp.diagnostic.set_loclist()<CR>")
+  set("n", "<C-k>f", "<cmd>lua vim.lsp.buf.formatting()<CR>")
 end
 
 
