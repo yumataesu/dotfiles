34e030a2d99418828999cc68322d3faa7aaaf7d2
yumataesu <yuma.taesu@gmail.com>
2022-08-30 17:49:28 +0900

    update : external file

--- .config/nvim/lua/nqmq/plugins.lua
+++ .config/nvim/lua/nqmq/plugins.lua
@@ -109,36 +109,3 @@ cmp.setup({
 })
 
 
-
-require('gitsigns').setup{}
-
-
-
--- require('fzf').setup()
-require('nvim-treesitter.configs').setup{
-  highlight = {
-    enable = true,
-  }
-}
-
-
-
-require("nvim-tree").setup({
-  sort_by = "case_sensitive",
-  view = {
-    adaptive_size = true,
-    mappings = {
-      list = {
-        { key = "u", action = "dir_up" },
-      },
-    },
-  },
-  renderer = {
-    group_empty = true,
-  },
-  filters = {
-    dotfiles = true,
-  },
-})
-
-
