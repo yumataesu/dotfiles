547d7443a4b02fdb0c333697f1acfef5fa1e0bab
yumataesu <yuma.taesu@gmail.com>
2022-08-30 17:51:35 +0900

    del

--- .config/nvim/lua/nqmq/plugins.lua
+++ .config/nvim/lua/nqmq/plugins.lua
@@ -72,18 +72,6 @@ local on_attach = function(client, bufnr)
   set("n", "<C-k>f", "<cmd>lua vim.lsp.buf.formatting()<CR>")
 end
 
-
-require("mason").setup()
-require("mason-lspconfig").setup()
-require("mason-lspconfig").setup_handlers {
-  function (server_name) -- default handler (optional)
-    require("lspconfig")[server_name].setup {
-      on_attach = on_attach,
-      capabilities = require("cmp_nvim_lsp").update_capabilities(vim.lsp.protocol.make_client_capabilities())
-    }
-  end,
-}
-
 vim.opt.completeopt = "menu,menuone,noselect"
 
 local cmp = require"cmp"
