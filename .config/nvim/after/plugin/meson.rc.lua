9d61e9de11608c4724ea0b32e3c48440954620e2
yumataesu <yuma.taesu@gmail.com>
2022-09-01 00:05:36 +0900

    Revert "update"
    This reverts commit ea05a706b98b33006bb8013a249835248fab41fc.

--- .config/nvim/after/plugin/meson.rc.lua
+++ .config/nvim/after/plugin/meson.rc.lua
@@ -1,1 +1,10 @@
-
+require("mason").setup()
+require("mason-lspconfig").setup()
+require("mason-lspconfig").setup_handlers {
+  function (server_name) -- default handler (optional)
+    require("lspconfig")[server_name].setup {
+      on_attach = on_attach,
+      capabilities = require("cmp_nvim_lsp").update_capabilities(vim.lsp.protocol.make_client_capabilities())
+    }
+  end,
+}
